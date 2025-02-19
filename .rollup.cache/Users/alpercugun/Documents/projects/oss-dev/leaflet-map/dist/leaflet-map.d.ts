6e4c730c149317eea07f4e6c902ff69f4220e87d
Alper Cugun <github@alper.nl>
2025-02-19 21:22:04 +0100

    Remove rollup cache

--- .rollup.cache/Users/alpercugun/Documents/projects/oss-dev/leaflet-map/dist/leaflet-map.d.ts
+++
@@ -1,7 +0,0 @@
-import { LeafletMap } from './src/LeafletMap.js';
-declare global {
-    interface HTMLElementTagNameMap {
-        'leaflet-map': LeafletMap;
-    }
-}
-//# sourceMappingURL=leaflet-map.d.ts.map
--- .rollup.cache/Users/alpercugun/Documents/projects/oss-dev/leaflet-map/dist/leaflet-map.js
+++
@@ -1,3 +0,0 @@
-import { LeafletMap } from './src/LeafletMap.js';
-window.customElements.define('leaflet-map', LeafletMap);
-//# sourceMappingURL=leaflet-map.js.map
